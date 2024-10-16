ad35cb005d37b9252a9e3eeb2a287e8b96ebac34
tanguypouriel <pourieltanguy@gmail.com>
2024-10-16 18:09:31 +0200

    Remove useless static site of textGeneration function. WIP for game setup

--- functions/textGeneration/src/main.js
+++ functions/textGeneration/src/main.js
@@ -1,30 +1,27 @@
 import { HfInference } from '@huggingface/inference';
-import { getStaticFile, throwIfMissing } from './utils.js';
+import { throwIfMissing } from './utils.js';
 
 export default async ({ req, res }) => {
   throwIfMissing(process.env, ['HUGGINGFACE_ACCESS_TOKEN']);
 
-  if (req.method === 'GET') {
-    return res.text(getStaticFile('index.html'), 200, {
-      'Content-Type': 'text/html; charset=utf-8',
-    });
-  }
+  if (req.method === 'GET') return res.json({ ok: false }, 400);
 
-  if (!req.bodyJson.prompt) {
-    return res.json({ ok: false, error: 'Prompt is required.' }, 400);
-  }
+  if (!req.bodyJson.prompt) return res.json({ ok: false, error: 'Prompt is required.' }, 400);
 
   const hf = new HfInference(process.env.HUGGINGFACE_ACCESS_TOKEN);
 
   try {
-    const result = await hf.textGeneration({
-      model: 'mistralai/Mistral-7B-Instruct-v0.2',
-      inputs: req.bodyJson.prompt,
-      max_new_tokens: req.bodyJson.max_new_tokens || 40,
-    });
-
-    return res.json({ ok: true, completion: result.generated_text }, 200);
+    let response = await hf.chatCompletion({
+      model: "Qwen/Qwen2.5-72B-Instruct",
+      messages: [
+        { role: "system", content: "You are participating in a game where you chose the character 'Saitama'. The user will ask you question to guess the character you chose. You can only anwer with Yes or No." },
+        { role: "user", content: req.bodyJson.prompt }],
+      max_tokens: 5,
+    })
+    
+    return res.json({ ok: true, completion: response.choices[0].message }, 200);
   } catch (err) {
+    console.error(err);
     return res.json({ ok: false, error: 'Failed to query model.' }, 500);
   }
 };
