a1c06aeebb21b52759da7a843bf4f7ead0c15200
tanguypouriel <pourieltanguy@gmail.com>
2024-10-21 18:09:29 +0200

    Update leaderboard ui for web

--- lib/screens/leaderboard/leaderboard_view.dart
+++ lib/screens/leaderboard/leaderboard_view.dart
@@ -2,8 +2,10 @@ import 'dart:ui';
 
 import 'package:askinator/di/service_locator.dart';
 import 'package:askinator/misc/color_theme.dart';
+import 'package:askinator/misc/responsive_layout_builder.dart';
 import 'package:askinator/screens/leaderboard/leaderboard_viewmodel.dart';
 import 'package:askinator/services/navigation_service.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:stacked/stacked.dart';
@@ -17,13 +19,16 @@ class LeaderboardView extends StatelessWidget {
       viewModelBuilder: () => sl<LeaderboardViewModel>(),
       onViewModelReady: (viewModel) async => await viewModel.initialise(),
       builder: (context, viewModel, child) => Container(
-        decoration: const BoxDecoration(
+        decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('assets/background.jpg'),
+            image: const AssetImage('assets/background.jpg'),
             opacity: 0.4,
-            alignment: Alignment(0.25, 0),
-            fit: BoxFit.fitHeight,
+            alignment: const Alignment(0.25, 0),
+            fit: MediaQuery.sizeOf(context).width > ResponsiveLayoutBuilder.thresholdWidth
+                ? BoxFit.cover
+                : BoxFit.fitHeight,
           ),
+          color: ColorTheme.theme.background.withOpacity(0.4),
         ),
         child: Scaffold(
           backgroundColor: Colors.transparent,
@@ -31,6 +36,7 @@ class LeaderboardView extends StatelessWidget {
             child: SingleChildScrollView(
               child: Column(
                 children: [
+                  kIsWeb ? const SizedBox(height: 24) : const SizedBox(),
                   Row(
                     children: [
                       Padding(
@@ -105,75 +111,84 @@ class LeaderboardView extends StatelessWidget {
                       ),
                     ),
                   ),
-                  ListView.builder(
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    itemCount: viewModel.leaderboardEntries.length,
-                    itemBuilder: (context, index) {
-                      final entry = viewModel.leaderboardEntries.elementAtOrNull(index);
+                  Container(
+                    width: MediaQuery.sizeOf(context).width > ResponsiveLayoutBuilder.thresholdWidth
+                        ? MediaQuery.sizeOf(context).width * 0.7
+                        : MediaQuery.sizeOf(context).width,
+                    decoration: kIsWeb ? BoxDecoration(
+                      border: Border.all(color: Colors.white, width: 2),
+                      borderRadius: BorderRadius.circular(36),
+                    ) : null,
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: viewModel.leaderboardEntries.length,
+                      itemBuilder: (context, index) {
+                        final entry = viewModel.leaderboardEntries.elementAtOrNull(index);
 
-                      bool isSelf = index == 3;
+                        bool isSelf = index == 3;
 
-                      if (entry == null) return const SizedBox();
+                        if (entry == null) return const SizedBox();
 
-                      return Container(
-                        decoration: BoxDecoration(
-                          color: isSelf ? ColorTheme.theme.primary.withOpacity(0.9) : Colors.transparent,
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 0),
-                        child: ListTile(
-                          contentPadding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
-                          leading: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: CircleAvatar(
-                              backgroundColor: ColorTheme.theme.secondary,
-                              child: Text(
-                                (index + 1).toString(),
-                                style: GoogleFonts.shadowsIntoLight().copyWith(
-                                  color: Colors.white,
-                                  fontSize: 26,
-                                  fontWeight: FontWeight.w600,
+                        return Container(
+                          decoration: BoxDecoration(
+                            color: isSelf ? ColorTheme.theme.primary.withOpacity(0.9) : Colors.transparent,
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 0),
+                          child: ListTile(
+                            contentPadding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+                            leading: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: CircleAvatar(
+                                backgroundColor: ColorTheme.theme.secondary,
+                                child: Text(
+                                  (index + 1).toString(),
+                                  style: GoogleFonts.shadowsIntoLight().copyWith(
+                                    color: Colors.white,
+                                    fontSize: 26,
+                                    fontWeight: FontWeight.w600,
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                          title: Text(
-                            entry.playerName,
-                            style: GoogleFonts.shadowsIntoLight().copyWith(
-                              color: Colors.white,
-                              fontSize: 30,
-                              fontWeight: FontWeight.w600,
+                            title: Text(
+                              entry.playerName,
+                              style: GoogleFonts.shadowsIntoLight().copyWith(
+                                color: Colors.white,
+                                fontSize: 30,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            trailing: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                switch (index) {
+                                  0 => Image.asset('assets/medal-first.png', height: 36),
+                                  1 => Image.asset('assets/medal-second.png', height: 36),
+                                  2 => Image.asset('assets/medal-third.png', height: 36),
+                                  _ => const SizedBox(),
+                                },
+                                const SizedBox(width: 24),
+                                Text(
+                                  '200',
+                                  style: GoogleFonts.shadowsIntoLight().copyWith(
+                                    color: ColorTheme.theme.secondary.withOpacity(0.8),
+                                    fontSize: 36,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                                const SizedBox(width: 12),
+                                Image.asset(
+                                  width: 36,
+                                  'assets/candy-rotated.png',
+                                ),
+                              ],
                             ),
                           ),
-                          trailing: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              switch (index) {
-                                0 => Image.asset('assets/medal-first.png', height: 36),
-                                1 => Image.asset('assets/medal-second.png', height: 36),
-                                2 => Image.asset('assets/medal-third.png', height: 36),
-                                _ => const SizedBox(),
-                              },
-                              const SizedBox(width: 24),
-                              Text(
-                                '200',
-                                style: GoogleFonts.shadowsIntoLight().copyWith(
-                                  color: ColorTheme.theme.secondary.withOpacity(0.8),
-                                  fontSize: 36,
-                                  fontWeight: FontWeight.w600,
-                                ),
-                              ),
-                              const SizedBox(width: 12),
-                              Image.asset(
-                                width: 36,
-                                'assets/candy-rotated.png',
-                              ),
-                            ],
-                          ),
-                        ),
-                      );
-                    },
+                        );
+                      },
+                    ),
                   ),
                 ],
               ),
